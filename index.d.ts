d601051a89cf815a739665d7f217511a68e99c1e
Elisée Maurer <elisee@sparklinlabs.com>
2016-01-18 04:42:16 +0100

    Include EventEmitter methods in typings, fix indentation

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,29 @@
 declare class TreeView {
-    treeRoot: HTMLOListElement;
-    selectedNodes: HTMLLIElement[];
+  treeRoot: HTMLOListElement;
+  selectedNodes: HTMLLIElement[];
 
-    constructor(container: HTMLElement, options?: { dropCallback?: TreeView.DropCallback, multipleSelection?: boolean });
-    clearSelection(): void;
-    addToSelection(element: HTMLLIElement): void;
-    append(element: HTMLLIElement, type: string /* "item" or "group" */, parentGroupElement?: HTMLElement): void;
-    insertBefore(element: HTMLLIElement, type: string, referenceElement: HTMLLIElement): void;
-    insertAt(element: HTMLLIElement, type: string, index: number, parentElement?: HTMLLIElement): void;
-    remove(element: HTMLLIElement): void;
-  }
+  constructor(container: HTMLElement, options?: { dropCallback?: TreeView.DropCallback, multipleSelection?: boolean });
+  clearSelection(): void;
+  addToSelection(element: HTMLLIElement): void;
+  append(element: HTMLLIElement, type: string /* "item" or "group" */, parentGroupElement?: HTMLElement): void;
+  insertBefore(element: HTMLLIElement, type: string, referenceElement: HTMLLIElement): void;
+  insertAt(element: HTMLLIElement, type: string, index: number, parentElement?: HTMLLIElement): void;
+  remove(element: HTMLLIElement): void;
+
+  addListener(event: string, listener: Function): TreeView;
+  on(event: string, listener: Function): TreeView;
+  once(event: string, listener: Function): TreeView;
+  removeListener(event: string, listener: Function): TreeView;
+  removeAllListeners(event?: string): TreeView;
+  setMaxListeners(n: number): TreeView;
+  getMaxListeners(): number;
+  listeners(event: string): Function[];
+  emit(event: string, ...args: any[]): boolean;
+  listenerCount(type: string): number;
+
+  on(event: "selectionChange", listener: () => any): TreeView;
+  on(event: "activate", listener: () => any): TreeView;
+}
 
 declare namespace TreeView {
   interface DropCallback {
