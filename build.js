690ab22337defa9fff1e98f7af1a2fc8b21c5c78
Elisée Maurer <elisee@sparklinlabs.com>
2016-01-20 01:28:36 +0100

    Fix build.js and update to TypeScript 1.7.5

--- build.js
+++ build.js
@@ -8,7 +8,10 @@ const watchMode = process.argv[2] === "-w";
 const bundler = watchMode ? "watchify" : "browserify";
 const watchArgs = watchMode ? [ "-w" ] : []; 
 
-spawn(`tsc${suffix}`, watchArgs.concat([ "-p", `${__dirname}/src` ]), spawnOptions);
 spawn(`jade${suffix}`, watchArgs.concat([ `${__dirname}/src/index.jade`, "--out", `${__dirname}/lib` ]), spawnOptions);
 spawn(`stylus${suffix}`, watchArgs.concat([ `${__dirname}/src/index.styl`, "--out", `${__dirname}/lib` ]), spawnOptions);
-spawn(`${bundler}${suffix}`, [ `${__dirname}/src/index.js`, "-s", "TreeView", "-o", `${__dirname}/lib/TreeView.js` ], spawnOptions);
+
+spawn(`tsc${suffix}`, [ "-p", `${__dirname}/src` ], spawnOptions).on("close", () => {
+  if (watchMode) spawn(`tsc${suffix}`, watchArgs.concat([ "-p", `${__dirname}/src` ]), spawnOptions);
+  spawn(`${bundler}${suffix}`, [ `${__dirname}/src/index.js`, "-s", "TreeView", "-o", `${__dirname}/lib/TreeView.js` ], spawnOptions);
+});
